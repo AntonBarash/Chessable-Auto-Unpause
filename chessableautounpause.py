1506b39e6f2f09df0eedcae17fe001ca77732d9d
AntonBarash <64368628+AntonBarash@users.noreply.github.com>
2021-08-26 02:16:45 -0400

    New Login Page and Var Click
    New login page so new login button has to be found instead of the old one, and .click doesnt work on the variation elements anymore for some reason, so executed script of clicking instead which works.

--- chessableautounpause.py
+++ chessableautounpause.py
@@ -11,19 +11,20 @@ inputemail = driver.find_element(By.NAME, "email")
 inputemail.send_keys("youremailhere") #replace youremailhere with your chessable email
 inputpass = driver.find_element(By.NAME, "password")
 inputpass.send_keys("yourpasswordhere") #replace yourpasswordhere with your chessable password
-login = driver.find_element(By.ID, "doLoginButton")
+#login = driver.find_element(By.ID, "loginButton") #deprecated button from old version of the website
+login = driver.find_element_by_xpath("//button[@data-test-id='loginButton']")
 login.click()
 time.sleep(1)
 driver.get("chessablecourselink") # replace chessablecourselink with the link for the chessable course and variation where you would like to unpause
 time.sleep(5)
 containing = "containstring" #replace containstring with the text that all the variations that you would like to unpause contain
-#elem=driver.find_element_by_xpath("//a[@title='Sicilian: 2...d5']")
 allvarelems = driver.find_elements_by_xpath("//*[contains(text(), '" + containing + "')]")
 for elem in allvarelems:
     href = elem.get_attribute("href")
     varid = "v" + href[36:]
     varid = varid[:-1]
     elem=driver.find_element_by_xpath("//li[@id='" + varid + "']/div[1]/div/div/span/i")
-    elem.click()
+    driver.execute_script("arguments[0].click();", elem) 
+    #elem.click() #deprecated, seems to not work now for some reason due to not clickable exception, so above execute script is used instead
 driver.close()
 driver.quit()
